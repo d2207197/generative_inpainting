ac219514ceb07e4297dd2db9ff91d53fbe835f1c
顏孜羲 <joseph.yen@gmail.com>
2018-08-18 22:11:28 +0800

    Inpainting add cache

--- inpainting.py
+++ inpainting.py
@@ -1,31 +1,37 @@
-import argparse
 
-import cv2
 import numpy as np
 import tensorflow as tf
-import neuralgym as ng
-
 from inpaint_model import InpaintCAModel
 
 
 class Inpainting(object):
+    _model_cache = {}
+
+    @classmethod
+    def from_checkpoint_dir(cls, checkpoint_dir):
+        if checkpoint_dir in cls._model_cache:
+            return cls._model_cache
+        else:
+            model = Inpainting(checkpoint_dir)
+            cls._model_cache[checkpoint_dir] = model
+            return model
+
     def __init__(self, checkpoint_dir):
         self.checkpoint_dir = checkpoint_dir
 
     @staticmethod
     def gen_mask(img_array):
-        xx, yy = np.where((img_array[:, :, 0] == 0) & (img_array[:, :, 1] == 255) & (img_array[:, :, 2] == 0))
-        #print(xx, yy)
+        xx, yy = np.where((img_array[:, :, 0] == 0) & (
+            img_array[:, :, 1] == 255) & (img_array[:, :, 2] == 0))
         mask = np.zeros(img_array.shape, dtype=int)
         for i, j in zip(xx, yy):
             mask[i, j, :] = [255, 255, 255]
         return mask
 
     def predict(self, image):
-        #ng.get_gpus(1)
+        # ng.get_gpus(1)
 
         model = InpaintCAModel()
-        #image = cv2.imread(args.image)
         mask = self.gen_mask(image)
 
         assert image.shape == mask.shape
@@ -48,19 +54,17 @@ class Inpainting(object):
             output = (output + 1.) * 127.5
             output = tf.reverse(output, [-1])
             output = tf.saturate_cast(output, tf.uint8)
+
             # load pretrained model
             vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
             assign_ops = []
             for var in vars_list:
                 vname = var.name
                 from_name = vname
-                var_value = tf.contrib.framework.load_variable(self.checkpoint_dir, from_name)
+                var_value = tf.contrib.framework.load_variable(
+                    self.checkpoint_dir, from_name)
                 assign_ops.append(tf.assign(var, var_value))
             sess.run(assign_ops)
             print('Model loaded.')
             result = sess.run(output)
-            #cv2.imwrite(args.output, result[0][:, :, ::-1])
             return result[0][:, :, ::-1]
-
-
-
