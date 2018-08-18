6dd6e8efd2910c096f2b1adf060c76dc57ae6db8
顏孜羲 <joseph.yen@gmail.com>
2018-08-19 03:01:45 +0800

    display

--- inpainting.py
+++ inpainting.py
@@ -1,7 +1,73 @@
 
+import base64
+import os
+from io import BytesIO
+
+import attr
+import cv2
 import numpy as np
+import requests
+import skimage.io as ski_io
 import tensorflow as tf
 from inpaint_model import InpaintCAModel
+from matplotlib import pyplot as plt
+
+output_p = "examples/output.png"
+checkpoint_dir_p = "model_logs/coffee"
+
+model = InpaintCAModel()
+
+
+def display_image_BRG(image_brg):
+    image_rgb = convert_BGR_to_RGB(image_brg)
+    return display_image_RGB(image_rgb)
+
+
+def display_image_RGB(image_rgb):
+    plt.imshow(image_rgb)
+    return plt.show()
+
+
+def convert_BGR_to_RGB(image_brg):
+    image_rgb = cv2.cvtColor(image_brg, cv2.COLOR_BGR2RGB)
+    return image_rgb
+
+
+def convert_RGB_to_BGR(image_rgb):
+    image_brg = cv2.cvtColor(image_rgb, cv2.COLOR_RGB2BGR)
+    return image_brg
+
+
+@attr.s
+class FoodQuiz:
+    question_id = attr.ib()
+    raw_image = attr.ib()
+    bbox = attr.ib()
+    description = attr.ib()
+
+
+cat_id_to_cat_map = {
+    0: '小籠包',
+    1: '麵包',
+    2: '義大利麵',
+    3: '沙拉',
+    4: '串燒',
+    5: '牛排',
+    6: '拉麵',
+    7: '壽司',
+    8: '丼飯',
+    9: '牛肉麵',
+    10: '生魚片',
+    11: '鬆餅',
+    12: '蛋糕',
+    13: '滷肉飯',
+    14: '火鍋',
+    15: '手搖杯',
+    16: '咖啡',
+    17: '冰淇淋',
+    18: '薯條',
+    19: '漢堡',
+}
 
 cat_to_checkpoint_map = {
     '小籠包': 'model_logs/20180816142207530631_instance-1_pixfood20-dumpling_NORMAL_wgan_gp_pixfood20_dumpling',  # noqa: E501
@@ -31,9 +97,14 @@ class Inpainting(object):
     _sess_cache = {}
 
     @classmethod
+    def from_cat_id(cls, cat_id):
+        cat = cat_id_to_cat_map[cat_id]
+        checkpoint_dir = cat_to_checkpoint_map[cat]
+        return cls.from_checkpoint_dir(checkpoint_dir)
+
+    @classmethod
     def from_cat(cls, cat):
         checkpoint_dir = cat_to_checkpoint_map[cat]
-        print(checkpoint_dir)
         return cls.from_checkpoint_dir(checkpoint_dir)
 
     @classmethod
@@ -50,7 +121,7 @@ class Inpainting(object):
         sess_config = tf.ConfigProto()
         sess_config.gpu_options.allow_growth = True
         input_image_ph = tf.placeholder(
-                tf.float32, shape=(1, 256, 512, 3))
+            tf.float32, shape=(1, 256, 512, 3))
         output = model.build_server_graph(input_image_ph)
         output = (output + 1.) * 127.5
         output = tf.reverse(output, [-1])
@@ -64,9 +135,10 @@ class Inpainting(object):
         for var in vars_list:
             vname = var.name
             from_name = vname
-            var_value = tf.contrib.framework.load_variable(checkpoint_dir, from_name)
+            var_value = tf.contrib.framework.load_variable(
+                checkpoint_dir, from_name)
             assign_ops.append(tf.assign(var, var_value))
-        sess.run(assign_ops)  
+        sess.run(assign_ops)
         self.sess = sess
         self.graph = output
         self.placeholder = input_image_ph
@@ -94,13 +166,94 @@ class Inpainting(object):
         mask = np.expand_dims(mask, 0)
         input_image = np.concatenate([image, mask], axis=2)
 
-        result = self.sess.run(self.graph, feed_dict={self.placeholder: input_image})
+        result = self.sess.run(self.graph,
+                               feed_dict={self.placeholder: input_image})
         return result[0][:, :, ::-1]
+
+
+# 從 PIXNET 拿到比賽題目
+def get_image(question_id, img_header=True):
+    endpoint = 'http://pixnethackathon2018-competition.events.pixnet.net/api/question'  # noqa: E501
+    payload = dict(question_id=question_id, img_header=img_header)
+    print('Step 1: 從 PIXNET 拿比賽題目\n')
+    response = requests.get(endpoint, params=payload)
+
+    try:
+        data = response.json()['data']
+        question_id = data['question_id']
+        description = data['desc']
+        bbox = data['bounding_area']
+
+        # Assign image format
+        encoded_image = data['image']
+        raw_image = ski_io.imread(
+            BytesIO(base64.b64decode(
+                encoded_image[encoded_image.find(',')+1:]))
+        )
+        raw_image = cv2.cvtColor(raw_image, cv2.COLOR_RGB2BGR)
+        header = encoded_image[:encoded_image.find(',')]
+        if 'bmp' not in header:
+            raise ValueError('Image should be BMP format')
+
+        print('題號：', question_id)
+        print('文字描述：', description)
+        print('Bounding Box:', bbox)
+        print('影像物件：', type(raw_image), raw_image.dtype,
+              ', 影像大小：', raw_image.shape)
+
+        quiz = FoodQuiz(question_id, raw_image, bbox, description)
+
+    except Exception as err:
+        # Catch exceptions here...
+        print(data)
+        raise err
+
+    print('=====================')
+
+    return quiz
+
+# 上傳答案到 PIXNET
+
+
+def submit_image(image, question_id, key=None):
+    print('Step 3: 上傳答案到 PIXNET\n')
+
+    endpoint = 'http://pixnethackathon2018-competition.events.pixnet.net/api/answer'  # noqa: E501
+
+    if key is None:
+        key = os.environ.get('PIXNET_FOODAI_KEY')
+
+    # Assign image format
+    image_format = 'jpeg'
+    with BytesIO() as f:
+        ski_io.imsave(f, image, format_str=image_format)
+        f.seek(0)
+        data = f.read()
+        encoded_image = base64.b64encode(data)
+    image_b64string = 'data:image/{};base64,'.format(
+        image_format) + encoded_image.decode('utf-8')
+
+    payload = dict(question_id=question_id,
+                   key=key,
+                   image=image_b64string)
+    response = requests.post(endpoint, json=payload)
+    try:
+        rdata = response.json()
+        return rdata
+        if response.status_code == 200 and not rdata['error']:
+            print('上傳成功')
+        print('題號：', question_id)
+        print('回答截止時間：', rdata['data']['expired_at'])
+        print('所剩答題次數：', rdata['data']['remain_quota'])
+
+    except Exception as err:
+        print(rdata)
+        raise err
+    print('=====================')
 
 
 if __name__ == '__main__':
     import sys
-    import cv2
 
     print(f'loading {sys.argv[1]}')
     model = Inpainting.from_cat(sys.argv[1])
